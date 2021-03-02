1b6a3e7fc854093db626dc6d8b20cc4d22d1e980
Rafael Rossi <rafaelzrossibr@gmail.com>
2021-03-02 12:43:13 -0300

    pre-UI-update

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-*{
-	border: 0,
-	margin: 0,
-	padding: 0
+const commands = {
+	"SEND-SYN":  1,
+	"RCVD-SYN":  2,
+	"SEND-FIN":  3,
+	"RCVD-FIN":  4,
+	"SEND-ACK":  5,
+	"RCVD-ACK":  6,
+	"SEND-MSG":  7,
+	"RCVD-MSG":  8,
+	"OPEN"    :  9,
+	"CLOSE"   : 10,
+	"TIMEOUT" : 11,
 }
+Object.freeze(commands);
+
